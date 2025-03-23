ba836549446d96df65779b9c46fedb06607f6a62
Johnson Hsu <specialline4107@gmail.com>
2025-03-23 22:49:26 +0800

    Add requirememts.txt and settings.py

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,11 +1,12 @@
 import urllib.request
 import json
+from yt_concate.settings import API_KEY
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
 def get_all_video_in_channel(channel_id):
-    api_key = 'AIzaSyCY53wi1JCj8HBtipuStqLpKqjHRUg7Bcg'
+    api_key = API_KEY
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
@@ -32,4 +33,4 @@ def get_all_video_in_channel(channel_id):
 
 
 video_list = get_all_video_in_channel(CHANNEL_ID)
-print(len(video_list))
+print(video_list)
