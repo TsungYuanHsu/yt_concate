9fbcc899021fe561928c0e1b1b1503df2fefd8b1
Johnson Hsu <specialline4107@gmail.com>
2025-03-26 23:41:38 +0800

    refactor code into pipeline design pattern

---
+++ yt_concate/pipeline/steps/step.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta
+from abc import abstractmethod
+
+
+class Step(metaclass=ABCMeta):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def process(self, data, inputs):
+        pass
+
+
+class StepException(Exception):
+    pass
+
